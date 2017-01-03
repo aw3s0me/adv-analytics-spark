0b58833e81d5bf2cf81eeb6fb6aa1942ac777b0d
aw3s0me <akorovin00@gmail.com>
2017-01-03 07:45:26 +0100

    evaluating categorical features with random forest

--- code/src/main/java/DecisionTree.scala
+++ code/src/main/java/DecisionTree.scala
@@ -316,4 +316,69 @@ class DecisionTree(private val spark: SparkSession) {
     val testAccuracy = multiclassEval.evaluate(bestModel.transform(unencTestData))
     println(testAccuracy)
   }
+
+  /**
+    * Building and evaluating of random forest
+    * @param trainData
+    * @param testData
+    */
+  def evaluateForest(trainData: DataFrame, testData: DataFrame): Unit = {
+    val unencTrainData = unencodeOneHot(trainData)
+    val unencTestData = unencodeOneHot(testData)
+
+    val assembler = new VectorAssembler().
+      setInputCols(unencTrainData.columns.filter(_ != "Cover_Type")).
+      setOutputCol("featureVector")
+
+    val indexer = new VectorIndexer().
+      setMaxCategories(40).
+      setInputCol("featureVector").
+      setOutputCol("indexedVector")
+
+    val classifier = new RandomForestClassifier().
+      setSeed(Random.nextLong()).
+      setLabelCol("Cover_Type").
+      setFeaturesCol("indexedVector").
+      setPredictionCol("prediction").
+      setImpurity("entropy").
+      setMaxDepth(20).
+      setMaxBins(300)
+
+    val pipeline = new Pipeline().setStages(Array(assembler, indexer, classifier))
+
+    val paramGrid = new ParamGridBuilder().
+      addGrid(classifier.minInfoGain, Seq(0.0, 0.05)).
+      // new parameter - number of trees
+      addGrid(classifier.numTrees, Seq(1, 10)).
+      build()
+
+    val multiclassEval = new MulticlassClassificationEvaluator().
+      setLabelCol("Cover_Type").
+      setPredictionCol("prediction").
+      setMetricName("accuracy")
+
+    val validator = new TrainValidationSplit().
+      setSeed(Random.nextLong()).
+      setEstimator(pipeline).
+      setEvaluator(multiclassEval).
+      setEstimatorParamMaps(paramGrid).
+      setTrainRatio(0.9)
+
+    val validatorModel = validator.fit(unencTrainData)
+
+    val bestModel = validatorModel.bestModel
+
+    val forestModel = bestModel.asInstanceOf[PipelineModel].
+      stages.last.asInstanceOf[RandomForestClassificationModel]
+
+    println(forestModel.extractParamMap)
+    println(forestModel.getNumTrees)
+    forestModel.featureImportances.toArray.zip(unencTrainData.columns).
+      sorted.reverse.foreach(println)
+
+    val testAccuracy = multiclassEval.evaluate(bestModel.transform(unencTestData))
+    println(testAccuracy)
+
+    bestModel.transform(unencTestData.drop("Cover_Type")).select("prediction").show()
+  }
 }
