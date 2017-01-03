b1fce5048c254741605d2c746b37d4a225fdbdd8
aw3s0me <akorovin00@gmail.com>
2017-01-03 10:58:26 +0100

    calculating clustering score by using average entropy

--- code/src/main/java/KMeansRun.scala
+++ code/src/main/java/KMeansRun.scala
@@ -242,4 +242,77 @@ class KMeansRun(private val spark: SparkSession) {
   def clusteringTake3(data: DataFrame): Unit = {
     (60 to 270 by 30).map(k => (k, clusteringScore3(data, k))).foreach(println)
   }
+
+  // Clustering, Take 4. Calculate cluster score
+  def entropy(counts: Iterable[Int]): Double = {
+    val values = counts.filter(_ > 0)
+    val n = values.map(_.toDouble).sum
+    values.map { v =>
+      val p = v / n
+      -p * math.log(p)
+    }.sum
+  }
+
+  def fitPipeline4(data: DataFrame, k: Int): PipelineModel = {
+    val (protoTypeEncoder, protoTypeVecCol) = oneHotPipeline("protocol_type")
+    val (serviceEncoder, serviceVecCol) = oneHotPipeline("service")
+    val (flagEncoder, flagVecCol) = oneHotPipeline("flag")
+
+    // Original columns, without label / string columns, but with new vector encoded cols
+    val assembleCols = Set(data.columns: _*) --
+      Seq("label", "protocol_type", "service", "flag") ++
+      Seq(protoTypeVecCol, serviceVecCol, flagVecCol)
+    val assembler = new VectorAssembler().
+      setInputCols(assembleCols.toArray).
+      setOutputCol("featureVector")
+
+    val scaler = new StandardScaler()
+      .setInputCol("featureVector")
+      .setOutputCol("scaledFeatureVector")
+      .setWithStd(true)
+      .setWithMean(false)
+
+    val kmeans = new KMeans().
+      setSeed(Random.nextLong()).
+      setK(k).
+      setPredictionCol("cluster").
+      setFeaturesCol("scaledFeatureVector").
+      setMaxIter(40).
+      setTol(1.0e-5)
+
+    val pipeline = new Pipeline().setStages(
+      Array(protoTypeEncoder, serviceEncoder, flagEncoder, assembler, scaler, kmeans))
+    pipeline.fit(data)
+  }
+
+  def clusteringScore4(data: DataFrame, k: Int): Double = {
+    val pipelineModel = fitPipeline4(data, k)
+
+    // Predict cluster for each datum
+    val clusterLabel = pipelineModel.transform(data).
+      select("cluster", "label").as[(Int, String)]
+    val weightedClusterEntropy = clusterLabel.
+      // Extract collections of labels, per cluster
+      groupByKey { case (cluster, _) => cluster }.
+      mapGroups { case (_, clusterLabels) =>
+        val labels = clusterLabels.map { case (_, label) => label }.toSeq
+        // Count labels in collections
+        val labelCounts = labels.groupBy(identity).values.map(_.size)
+        labels.size * entropy(labelCounts)
+      }.collect()
+
+    // Average entropy weighted by cluster size
+    weightedClusterEntropy.sum / data.count()
+  }
+
+  def clusteringTake4(data: DataFrame): Unit = {
+    (60 to 270 by 30).map(k => (k, clusteringScore4(data, k))).foreach(println)
+
+    val pipelineModel = fitPipeline4(data, 180)
+    val countByClusterLabel = pipelineModel.transform(data).
+      select("cluster", "label").
+      groupBy("cluster", "label").count().
+      orderBy("cluster", "label")
+    countByClusterLabel.show()
+  }
 }
