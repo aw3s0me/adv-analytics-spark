30be6dc6ae98d12249ca173783e87c350beed575
aw3s0me <akorovin00@gmail.com>
2017-01-03 10:24:17 +0100

    dealing with categorical values kmeans one-hot encoding

--- code/src/main/java/KMeansRun.scala
+++ code/src/main/java/KMeansRun.scala
@@ -184,4 +184,62 @@ class KMeansRun(private val spark: SparkSession) {
     (60 to 270 by 30).map(k => (k, clusteringScore2(numericOnly, k))).foreach(println)
     numericOnly.unpersist()
   }
+
+  // Clustering, Take 3. Dealing with categorical features
+  /**
+    * Function to convert categorical features (protocol service flag) to numerical features
+    * Using one-hot encoding
+    * @param inputCol
+    * @return
+    */
+  def oneHotPipeline(inputCol: String): (Pipeline, String) = {
+    val indexer = new StringIndexer().
+      setInputCol(inputCol).
+      setOutputCol(inputCol + "_indexed")
+    // use predefined class. OneHotEncoder
+    val encoder = new OneHotEncoder().
+      setInputCol(inputCol + "_indexed").
+      setOutputCol(inputCol + "_vec")
+    val pipeline = new Pipeline().setStages(Array(indexer, encoder))
+    (pipeline, inputCol + "_vec")
+  }
+
+  def clusteringScore3(data: DataFrame, k: Int): Double = {
+    val (protoTypeEncoder, protoTypeVecCol) = oneHotPipeline("protocol_type")
+    val (serviceEncoder, serviceVecCol) = oneHotPipeline("service")
+    val (flagEncoder, flagVecCol) = oneHotPipeline("flag")
+
+    // Original columns, without label / string columns, but with new vector encoded cols
+    val assembleCols = Set(data.columns: _*) --
+      Seq("label", "protocol_type", "service", "flag") ++
+      Seq(protoTypeVecCol, serviceVecCol, flagVecCol)
+    val assembler = new VectorAssembler().
+      setInputCols(assembleCols.toArray).
+      setOutputCol("featureVector")
+
+    val scaler = new StandardScaler()
+      .setInputCol("featureVector")
+      .setOutputCol("scaledFeatureVector")
+      .setWithStd(true)
+      .setWithMean(false)
+
+    val kmeans = new KMeans().
+      setSeed(Random.nextLong()).
+      setK(k).
+      setPredictionCol("cluster").
+      setFeaturesCol("scaledFeatureVector").
+      setMaxIter(40).
+      setTol(1.0e-5)
+
+    val pipeline = new Pipeline().setStages(
+      Array(protoTypeEncoder, serviceEncoder, flagEncoder, assembler, scaler, kmeans))
+    val pipelineModel = pipeline.fit(data)
+
+    val kmeansModel = pipelineModel.stages.last.asInstanceOf[KMeansModel]
+    kmeansModel.computeCost(pipelineModel.transform(data)) / data.count()
+  }
+
+  def clusteringTake3(data: DataFrame): Unit = {
+    (60 to 270 by 30).map(k => (k, clusteringScore3(data, k))).foreach(println)
+  }
 }
